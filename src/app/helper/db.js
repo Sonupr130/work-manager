443c2a6ded520b05f203236ad00fcb7885b01eab
Sonupr130 <sonupradhan130@gmail.com>
2024-07-20 21:55:14 +0530

    creating a user model

--- src/app/helper/db.js
+++ src/app/helper/db.js
@@ -1,5 +1,6 @@
 
 import mongoose from "mongoose"
+import { User } from "../models/user";
 
 export const connectDb = async() => {
     try {
@@ -7,9 +8,21 @@ export const connectDb = async() => {
             dbName: "work_manager"
         });
 
+
         console.log("Databse Connected! 🔥");
         console.log(connection);
         // console.log("Connected with host ", connection.host);
+
+
+        // testing and creating user
+        const uuser = new User({
+            name: "test name",
+            email: "test@gmail.com",
+            password: "test233",
+            about: "this is simple testing"
+        });
+        await uuser.save();
+        console.log("User is Created");
     } catch (error) {
         console.log("Failed to connect with databse ❌");
         console.log(error);
