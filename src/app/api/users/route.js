5ce7ed9042c8a563030f4b9d98de0bf02323d284
Sonupr130 <sonupradhan130@gmail.com>
2024-07-24 23:50:38 +0530

    complete signup module

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -3,15 +3,23 @@ import { NextResponse } from "next/server";
 
 
 
-
-
-
-
-// CREATE USER API (http://localhost:3000/api/users)
 export async function POST(request) {
-  try {
+  
     // fetch user detail from request
     const { name, email, password, about, profileURL } = await request.json();
+
+
+        // Check if user with the same email already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return NextResponse.json(
+            {
+              message: "User with this email already exists",
+              status: false,
+            },
+            { status: 409 }
+          );
+        }
 
     // create user object with usermodel
     const user = new User({
@@ -22,59 +30,40 @@ export async function POST(request) {
       profileURL,
     });
 
+    try {
+
     // save the object to the database
     const createdUser = user.save();
     const response = NextResponse.json(user, {
       message: "User created successfully",
-      status: 200,
+      status: 201,
     });
     return response;
-
   } catch (error) {
-    console.error(error);
-    return NextResponse.json({
-      message: "Failed to Create user",
-      status: false,
-    });
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "Failed to Create user",
+        status: false,
+      },
+      { status: 500, }
+    );
   }
 }
 
-
-
-
-
 // GET ALL USER API (http://localhost:3000/api/users)
 export async function GET(req, res) {
-  
-
-    let users = [];
-    try {
-        // users = await User.find();
-        users = await User.find().select("-password");
-
-
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json({
-            message: "Failed to fetch users",
-            status: false,
-        });
-    }
+  let users = [];
+  try {
+    // users = await User.find();
+    users = await User.find().select("-password");
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      message: "Failed to fetch users",
+      status: false,
+    });
+  }
 
   return NextResponse.json(users);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/app/signup/SignUp.jsx
+++ src/app/signup/SignUp.jsx
@@ -1,54 +1,169 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import Signup from "../../assets/signup.svg"
+import Signup from "../../assets/signup.svg";
+import { toast } from "react-toastify";
+import { signUp } from "@/services/userService";
+
 
 const SignUp = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    about: "",
+    profileURL: "https://i.sstatic.net/YaL3s.jpg",
+  });
+
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+    console.log(data);
+
+    if (data.name.trim() === "" || data.name == null) {
+      toast.warning("Name is required!", { position: "top-center" });
+      return;
+    }
+
+
+    try {
+      const result = await signUp(data);
+      console.log(result);
+
+      toast.success("User signed up successfully", { position: "top-center" });
+
+      setData({
+        name: "",
+        email: "",
+        password: "",
+        about: "",
+        profileURL: "https://i.sstatic.net/YaL3s.jpg",
+      });
+    } catch (error) {
+        console.log(error);
+        console.log(error.response.data.message);
+        toast.error("Signup Error !! " + error.response.data.message, {
+          position: "top-center",
+        });
+      }
+    };
+
+  const resetForm = () => {
+    setData({
+      name: "",
+      email: "",
+      password: "",
+      about: "",
+      profileURL: "https://i.sstatic.net/YaL3s.jpg",
+    });
+  };
+
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-4 col-start-5">
         <div className="py-5">
-        <div className="m-5 flex justify-center">
-          <Image src={Signup} alt="signup-image" style={{ width: "50%" }} />
-        </div>
+          <div className="m-5 flex justify-center">
+            <Image src={Signup} alt="signup-image" style={{ width: "50%" }} />
+          </div>
           <h1 className="text-3xl text-center">SignUp Here</h1>
-          <form action="" className="mt-5">
+          <form action="" className="mt-5" onSubmit={handleSignUp}>
             <div className="mt-3">
-            {/* username  */}
-              <label className="block text-sm font-medium mb-2" htmlFor="user_name">Username</label>
-              <input type="text" placeholder="enter here" name=""   className="w-full p-3 rounded-md bg-[#d8cced] outline-none mb-2" />
+              {/* username  */}
+              <label
+                className="block text-sm font-medium mb-2"
+                htmlFor="user_name"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                placeholder="enter here"
+                name="user_name"
+                value={data.name}
+                onChange={(event) => {
+                  setData({ ...data, name: event.target.value });
+                }}
+                className="w-full p-3 rounded-md bg-[#d8cced] outline-none mb-2"
+              />
             </div>
-
 
             {/* email  */}
             <div>
-            <label className="block text-sm font-medium mb-2" htmlFor="user_email">Email</label>
-              <input type="email" placeholder="enter here" id="user_email"   className="w-full p-3 rounded-md bg-[#d8cced] outline-none mb-2" />
+              <label
+                className="block text-sm font-medium mb-2"
+                htmlFor="user_email"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={data.email}
+                onChange={(event) => {
+                  setData({ ...data, email: event.target.value });
+                }}
+                placeholder="enter here"
+                id="user_email"
+                className="w-full p-3 rounded-md bg-[#d8cced] outline-none mb-2"
+              />
             </div>
-
 
             {/* password  */}
             <div>
-            <label className="block text-sm font-medium mb-2" htmlFor="user_password">Password</label>
-              <input type="password" placeholder="enter here" id="user_password"   className="w-full p-3 rounded-md bg-[#d8cced] outline-none mb-2" />
+              <label
+                className="block text-sm font-medium mb-2"
+                htmlFor="user_password"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                placeholder="enter here"
+                name="password"
+                value={data.password}
+                onChange={(event) => {
+                  setData({ ...data, password: event.target.value });
+                }}
+                id="user_password"
+                className="w-full p-3 rounded-md bg-[#d8cced] outline-none mb-2"
+              />
             </div>
-
 
             {/* about section  */}
             <div>
-            <label className="block text-sm font-medium mb-2" htmlFor="user_about">About</label>
-            <textarea
-              rows={8}
-              id="user_about"
-              className="w-full p-3 rounded-md bg-[#d8cced] outline-none"
-              type="text"
-            ></textarea>
+              <label
+                className="block text-sm font-medium mb-2"
+                htmlFor="user_about"
+              >
+                About
+              </label>
+              <textarea
+                rows={8}
+                id="user_about"
+                name="user_about"
+                value={data.about}
+                onChange={(event) => {
+                  setData({ ...data, about: event.target.value });
+                }}
+                className="w-full p-3 rounded-md bg-[#d8cced] outline-none"
+                type="text"
+              ></textarea>
             </div>
 
             <div className="mt-3 space-x-5 text-center">
-                <button className="px-2 py-3 bg-green-600 rounded-md hover:bg-green-400">SignUp</button>
-                <button className="px-2 py-3 bg-orange-600 rounded-md hover:bg-orange-400">SignUp</button>
+              <button
+                type="submit"
+                className="px-2 py-3 bg-green-600 rounded-md hover:bg-green-400"
+              >
+                SignUp
+              </button>
+              <button
+                onClick={resetForm}
+                className="px-2 py-3 bg-orange-600 rounded-md hover:bg-orange-400"
+              >
+                Reset
+              </button>
+              {JSON.stringify(data)}
             </div>
           </form>
         </div>
